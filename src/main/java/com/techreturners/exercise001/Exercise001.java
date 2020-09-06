c4ef4a3fb57ee747970bd19e313b0a260be8673e
Jyothsna C Guvvala <jyothsna234@yahoo.com>
2020-09-06 10:09:59 +0100

    Implement exercise 001-capitalizeWord,generateInitials

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -4,16 +4,15 @@ import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+      return Character.toUpperCase(word.charAt(0)) + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        return "";
+        return Character.toUpperCase(firstName.charAt(0))+"."+Character.toUpperCase(lastName.charAt(0));
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        return 0.0;
+        return (originalPrice + double vatRate) ;
     }
 
     public String reverse(String sentence) {
