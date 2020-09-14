78b959f65caf542671ed96a95ba7c9b25bbf557c
Jyothsna C Guvvala <jyothsna234@yahoo.com>
2020-09-14 23:35:52 +0100

    Working on Exercise002

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -1,13 +1,17 @@
 package com.techreturners.exercise002;
 
 public class Exercise002 {
-
-    public boolean isFromManchester(/* Person person */) {
-        return false;
+  class Person {
+  private String firstName;
+  private String lastName;
+  private String pCity;
+  private int age;
+    public boolean isFromManchester(Person person ) {
+      if (Person.get(2).equals("Manchester")){return true;} else {return false;}
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        return false;
+    public boolean canWatchFilm(Person person, int ageLimit) {
+      if (Person.get(3)>18) {return true;} else {return false;}
     }
-    
+}
 }
